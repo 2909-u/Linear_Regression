770156d26e4d37225131ffb8390f993e80ec9515
2909-u <157318686+2909-u@users.noreply.github.com>
2024-12-18 10:40:03 +0500

    Update linear.py

--- linear.py
+++ linear.py
@@ -1,4 +1,10 @@
-import streamlit as st
+import os
+import subprocess
+
+try:
+    import matplotlib
+except ImportError:
+    subprocess.check_call(["pip", "install", "matplotlib"])
 import pandas as pd
 import numpy as np
 import matplotlib.pyplot as plt
